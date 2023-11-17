1507d83f63d93e6226b97b48325a2c7301811f20
colins-uky <colin.schuh@uky.edu>
2023-11-16 19:56:43 -0500

    Added gazebo simulation launch parameters to their own file.

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -9,7 +9,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
     package_name="minibot"
@@ -20,10 +19,15 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+
+    # Get gazebo params file from shared directory
+    gazebo_params_filepath = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={"extra_gazebo_args": "--ros-args --params-file " + gazebo_params_filepath}.items()
     )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
